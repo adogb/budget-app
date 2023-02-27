22adde69b1be3f5c202b4dbc07fba58ee16c10c2
adogb <13393572+adogb@users.noreply.github.com>
2023-02-27 16:52:30 +0100

    Make database out of dataframe

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import config
 import requests
 import re
+import sqlite3
 
 # extract transactions from each account a load into dataframe
 # for account in accounts:
@@ -19,21 +20,32 @@ df = pd.json_normalize(response_dict["transactions"]["booked"])
 df = df[["transactionId","bookingDate","remittanceInformationUnstructured",\
   "transactionAmount.amount","transactionAmount.currency","additionalInformation"]]
 
-df.rename(columns={"additionalInformation":"transac_type", "bookingDate":"booking_date", "remittanceInformationUnstructured":"desc_unstructured", "transactionAmount.amount":"amount", "transactionAmount.currency":"currency", "transactionId":"id"}, inplace=True)
+df.rename(columns={"additionalInformation":"trans_type", "bookingDate":"booking_date", "remittanceInformationUnstructured":"desc_unstructured", "transactionAmount.amount":"amount", "transactionAmount.currency":"currency", "transactionId":"id"}, inplace=True)
 
 df.desc_unstructured = df.desc_unstructured.str.split().str.join(" ")
 
 def extract_relevant(row):
-  if row.transac_type == "DANKORT-NOTA":
+  if row.trans_type == "DANKORT-NOTA":
     return row.desc_unstructured.replace("Dankort-nota ", "", 1)
-  elif (row.transac_type == "VISA KØB") | (row.transac_type == "VISA MODPOSTERING"):
+  elif (row.trans_type == "VISA KØB") | (row.trans_type == "VISA MODPOSTERING"):
     return re.search("\d+,\d\d (.+ Den \d\d\.\d\d)", row.desc_unstructured).group(1)
-  elif row.transac_type == "BGS":
+  elif row.trans_type == "BGS":
     return row.desc_unstructured.replace("Bgs ", "", 1)
-  elif row.transac_type == "MOBILEPAY":
+  elif row.trans_type == "MOBILEPAY":
     return row.desc_unstructured.replace("MobilePay ", "", 1)
   else:
     return row.desc_unstructured
 
 df["desc"] = df.apply(extract_relevant, axis=1)
+df = df[["id","booking_date","desc","desc_unstructured", "amount", "currency", "trans_type"]]
+
 # load into database
+con = sqlite3.connect("data.sqlite3") # returns a Connection Object. Extension is by choice, could be .db or .sqlite
+cur = con.cursor() # returns a Cursor Object using the connection
+cur.execute("DROP TABLE IF EXISTS Transactions")
+cur.execute("CREATE TABLE Transactions (trans_id TEXT PRIMARY KEY, booking_date TEXT, desc TEXT, desc_original TEXT, amount REAL, currency TEXT, type TEXT) WITHOUT ROWID") # in reality, flexible typing from SQLite does not require specifying data types
+cur.executemany("INSERT INTO Transactions VALUES(" + "?,"*(len(df.columns)-1) + "?)", df.to_numpy())
+con.commit()
+# SQLite uses ? as placeholder. Don't use string formatting to avoid SQL injection. df.to_numpy() gives a list of rows (themselves a list of values)
+con.close()
+
