72626f2681bdf04f1bdbc09beee8eedb62ee4090
adogb <13393572+adogb@users.noreply.github.com>
2023-05-08 19:39:57 +0200

    Separate database creation and population

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ headers = {
     'Authorization': 'Bearer '+config.access_token,
 }
 
-response = requests.get('https://ob.nordigen.com/api/v2/accounts/'+config.accounts_ids["grundkto"]+'/transactions/?date_from=2023-01-01&date_to=2023-02-20', headers=headers)
+response = requests.get('https://ob.nordigen.com/api/v2/accounts/'+config.accounts_ids["grundkto"]+'/transactions/?date_from=2023-05-01&date_to=2023-05-08', headers=headers)
 
 response_dict = response.json()
 df = pd.json_normalize(response_dict["transactions"]["booked"])
@@ -39,16 +39,17 @@ def extract_relevant(row):
 df["desc"] = df.apply(extract_relevant, axis=1)
 df = df[["id","booking_date","desc","desc_unstructured", "amount", "currency", "trans_type"]]
 
-# load into database
-con = sqlite3.connect("data.sqlite3") # returns a Connection Object. Extension is by choice, could be .db or .sqlite
+# Create database
+""" con = sqlite3.connect("data.sqlite3") # returns a Connection Object. Extension is by choice, could be .db or .sqlite
 cur = con.cursor() # returns a Cursor Object using the connection
-cur.execute("DROP TABLE IF EXISTS Transactions")
 cur.execute("CREATE TABLE Transactions (trans_id TEXT PRIMARY KEY, booking_date TEXT, desc TEXT, desc_original TEXT, amount REAL, currency TEXT, type TEXT) WITHOUT ROWID") # in reality, flexible typing from SQLite does not require specifying data types
-cur.executemany("INSERT INTO Transactions VALUES(" + "?,"*(len(df.columns)-1) + "?)", df.to_numpy())
-con.commit()
-
 cur.execute("CREATE TABLE Categories (cat_id INTEGER PRIMARY KEY, level INTEGER NOT NULL, parent_id INTEGER, name TEXT NOT NULL, desc TEXT, FOREIGN KEY(parent_id) REFERENCES Categories(cat_id))")
 cur.execute("CREATE TABLE Keywords (keyword TEXT NOT NULL, category INTEGER, FOREIGN KEY(category) REFERENCES Categories(cat_id))")
-# SQLite uses ? as placeholder. Don't use string formatting to avoid SQL injection. df.to_numpy() gives a list of rows (themselves a list of values)
-con.close()
+con.commit()
+con.close() """
+
+# Building the database
+con = sqlite3.connect("data.sqlite3")
+cur = con.cursor()
+cur.executemany("INSERT INTO Transactions VALUES(" + "?,"*(len(df.columns)-1) + "?)", df.to_numpy()) # SQLite uses ? as placeholder. Don't use string formatting to avoid SQL injection. df.to_numpy() gives a list of rows (themselves a list of values)
 
