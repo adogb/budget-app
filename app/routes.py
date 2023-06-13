ca1ed023647f423a6ba35b9590984b1f38f0bd3f
adogb <13393572+adogb@users.noreply.github.com>
2023-06-13 21:08:07 +0200

    Test communication front back

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,15 @@
 from app import app # second app is the variable defined in app\__init__.py
+import sqlite3
+from flask import render_template
+import pandas as pd
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    # retrieve categories from database
+    con = sqlite3.connect("data.sqlite3")
+    categories_df = pd.read_sql_query("SELECT * FROM Categories", con)
+    transactions_df = pd.read_sql_query("SELECT * FROM Transactions", con)
+    con.close()
+
+    # render html
+    return render_template("index.html", categories_df=categories_df, transactions_df=transactions_df)
