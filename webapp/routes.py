16616b3a40442f6a3991f7598eee0254a793ba04
adogb <13393572+adogb@users.noreply.github.com>
2023-10-25 20:11:14 +0200

    Add bar chart of expenses

--- webapp/routes.py
+++ webapp/routes.py
@@ -2,6 +2,7 @@ from webapp import app #  variable app
 import sqlite3
 from flask import render_template
 import pandas as pd
+import plotly.express as px
 
 @app.route('/')
 def index():
@@ -29,7 +30,22 @@ def index():
     transactions_df.sort_values(by="booking_date", ascending=False, inplace=True)
 
     # extract the transaction' month in a new column using datetime
-    transactions_df["month"] = pd.to_datetime(transactions_df.booking_date).dt.strftime("%B %Y")
+    transactions_df["month"] = pd.to_datetime(transactions_df.booking_date, format="%B %Y")
+
+    # create dataframe for bar chart
+    expenses_df = transactions_df[transactions_df['category'] != 'Salary']
+    incomes_df = transactions_df[transactions_df['category'] == 'Salary']
+
+    expenses_grouped = expenses_df.groupby(by=transactions_df['month'].dt.strftime('%B %Y'))['amount'].sum().reset_index()
+    incomes_grouped = incomes_df.groupby(by=transactions_df['month'].dt.strftime('%B %Y'))['amount'].sum().reset_index()
+
+    # Merge the two DataFrames on the 'Date' column to create a summary DataFrame
+    summary_df = expenses_grouped.merge(incomes_grouped, on='month', how='outer')
+    summary_df.columns = ['month', 'expenses', 'incomes']
+
+    # create bar chart of monthly expenses and incomes
+    fig = px.bar(summary_df, x="month", y="expenses")
+    graph_html = fig.to_html(include_plotlyjs='cdn')
 
     # render html
-    return render_template("index.html", transactions_df=transactions_df)
+    return render_template("index.html", transactions_df=transactions_df, graph=graph_html)
